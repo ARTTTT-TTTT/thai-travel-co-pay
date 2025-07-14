5f5bb97fbb695664a63d0baaf1299f6fb285b5ed
ARTTTT_TTTT <tanapath.tp@gmail.com>
2025-07-14 15:48:36 +0700

    fix(server): change file name

--- server/app/api.py
+++ server/app/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from app.routes import auth_route, user_route, user_travel, province_route
+
+from app.routes import auth_route, user_route, province_route, user_travel_route
 
 api_router = APIRouter()
 
 api_router.include_router(auth_route.router)
 api_router.include_router(user_route.router)
-api_router.include_router(user_travel.router)
+api_router.include_router(user_travel_route.router)
 api_router.include_router(province_route.router)
